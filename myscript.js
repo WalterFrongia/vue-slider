0e0f46eeb5e80855a21718f3d25035bb93265543
WalterFrongia <walter.frongia@libero.it>
2021-09-15 17:25:04 +0200

    Implementazione funzione per scorrere le img

--- myscript.js
+++ myscript.js
@@ -6,7 +6,31 @@ var app = new Vue(
             {
                 source:"https://mole24.it/wp-content/uploads/2019/11/space_adventure_torino.jpg",
                 alt:"",
-            }
-      ],
+            },
+            {
+                source:"https://images3.alphacoders.com/905/905078.jpg",
+                alt:"",
+            },
+            {
+                source:"https://images6.alphacoders.com/923/thumb-1920-923687.jpg",
+                alt:"",
+            },
+        ],
+        imageIndex: 0, //questa è la chiave dell'oggetto.
     },
+
+    methods: {
+        
+        //FUNZIONE PER SCORRERE LE IMG SIA QUELLA PRIMA CHE QUELLA DOPO, IMAGEINDEX VA MOTIFICATO ANCHE SU HTML PER DARE UN INDICE ALL'IMG.
+        previusImage: function(){   //questa funzione permette di scorrere l'array di uno aumentando di uno l'indice
+            this.imageIndex--;      //
+            console.log(this.imageIndex);
+        },
+
+        nextImage: function(){     //questa funzione permette di scorrere l'array di uno sottraendo di uno l'indice     
+            this.imageIndex++;
+            console.log(this.imageIndex);
+        },
+
+    }
 });
